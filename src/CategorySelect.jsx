e41145ccf6811a0e61ce1d746bca81841f487722
dkingbrandt <dkingbrandt@hotmail.com>
2022-05-20 19:33:24 +0200

    starting change to filter function

--- src/CategorySelect.jsx
+++ src/CategorySelect.jsx
@@ -3,10 +3,12 @@ import { useStates } from './utilities/states';
 export default function CategorySelector(props) {
 
   let s = useStates('main');
-  let { bindTo, showAllOption } = props;
+  let { bindTo,showAllOption } = props;
   let [bindObject, bindProperty] = bindTo;
 
   return <select {...bindObject.bind(bindProperty)}>
+    
+    {showAllOption &&<option key={0} value={0}>Alla Produkter</option>}
     
     {s.categories.map(({ name, id }) =>
       <option key={id} value={id}>{name}</option>
