dae57e50ab8d71b9005710ec2f1f0530ad15dee7
Annika Hägglund <annika.a.a.hagglund@outlook.com>
2022-05-23 19:13:47 +0200

    personal info page

--- src/components/BackOffice.jsx
+++ src/components/BackOffice.jsx
@@ -34,15 +34,19 @@ export default function backOffice() {
           <CategorySelect showAllOption bindTo={[s, "chosenCategoryId"]} />
         </Col>
       </Row>
-      {s.products
-        .filter(
-          (product) =>
-            s.chosenCategoryId === 0 /*all*/ ||
-            s.chosenCategoryId === product.categoryId
-        )
-        .map(({ id, name, description, price }) => (
-          <Row className="product" key={id} onClick={() => showDetail(id)}>
-            <Card>
+      <Row xs={2} md={4} lg={6}>
+        {s.products
+          .filter(
+            (product) =>
+              s.chosenCategoryId === 0 /*all*/ ||
+              s.chosenCategoryId === product.categoryId
+          )
+          .map(({ id, name, description, price }) => (
+            <Card
+              style={{ width: "20rem", margin: "0.25rem" }}
+              key={id}
+              onClick={() => showDetail(id)}
+            >
               <Col xxl="12">
                 <h3>{name}</h3>
                 <img
@@ -51,9 +55,9 @@ export default function backOffice() {
                   style={{ width: 300, height: "auto", objectFit: "cover" }}
                   src={`/images/products/${id}.jpg`}
                 />
-                <p>{description}</p>
+                <p className="mb-5">{description}</p>
               </Col>
-              <Col xxl="12">
+              <Col xxl="12" className="mt-5 position-absolute bottom-0 end-1">
                 <p>
                   <b>Pris:</b> {sweFormat(price)}
                 </p>
@@ -61,14 +65,14 @@ export default function backOffice() {
               <Link to={`/backoffice/${id}`}>
                 <button
                   type="button"
-                  className="my-4 btn btn-primary float-end"
+                  className="my-3  btn btn-primary position-absolute bottom-0 end-0"
                 >
                   Ändra
                 </button>
               </Link>
             </Card>
-          </Row>
-        ))}
+          ))}
+      </Row>
     </Container>
   );
 }
